ff4183778bd8f8c28a6a2853c992c2f44d8ad495
Kenny Elias <109461921+kennyegun24@users.noreply.github.com>
2022-12-24 10:37:33 +0100

    decode_morse code

--- decode_morse.rb
+++ decode_morse.rb
@@ -31,3 +31,21 @@ def decode_char(char)
   MORSE_CODE[char]
 end
 puts decode_char('.-')
+
+def decode_word(word)
+  split_word = ''
+  word.split.each { |n| split_word += decode_char(n) }
+  split_word
+end
+puts decode_word('-- -.--')
+
+
+def decode(sentence)
+  words = sentence.split('   ')
+  message = ''
+  words.each { |n| message += "#{decode_word(n)} " }
+  message
+end
+
+puts decode('-- -.--   -. .- -- .')
+puts decode(' .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
